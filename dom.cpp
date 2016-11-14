0a9fd4508d3b24832fae493b4cc397e49292387c
Elito <elito0o097@abv.bg>
2016-11-14 16:15:00 +0200

    Create dom.cpp

---
+++ dom.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<stack>
+#include<fstream>
+using namespace std;
+void PolskiZapis(ifstream& f)
+{
+
+	stack<int> st;
+	int a, b, c;
+	char p;
+	int n;
+	char op;
+	while (!f.eof())
+	{
+		p = f.peek();
+		if (p >= '0' && p <= '9')
+		{
+			f >> n;
+			st.push(n);
+			
+		}
+		else {
+			f >> op;
+			cout << op << " ";
+			switch (op)
+			{
+			case '+': {
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					a = st.top();
+					
+					st.pop();
+				}
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					b = st.top();
+					
+					st.pop();
+				}
+				c = a + b;
+
+				st.push(c);
+			}break;
+			case '-': {
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					a = st.top();
+					st.pop();
+				}
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					b = st.top();
+					st.pop();
+				}
+				c = a - b;
+				st.push(c);
+			}break;
+			case '/': {
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					a = st.top();
+					st.pop();
+				}
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					b = st.top();
+					st.pop();
+				}
+				c = a + b;
+				st.push(c);
+			}break;
+			case '*': {
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					a = st.top();
+					st.pop();
+				}
+				if (st.empty())
+				{
+					cout << "error!";
+				}
+				else
+				{
+					b = st.top();
+					st.pop();
+				}
+				c = a * b;
+				st.push(c);
+			}break;
+			}
+		}
+
+	}
+	while (!st.empty())
+	{
+		cout << st.top() << " ";
+		st.pop();
+	}
+
+}
+int main()
+{
+	ifstream f("exp.txt");
+	PolskiZapis(f);
+
+
+	system("pause");
+	return 0;
+}
